22543899c047965c2388703c826d7d5b8ea87057
Claudius <58847225+cruecker@users.noreply.github.com>
2021-07-20 16:22:05 +0200

    Create script.js

---
+++ 05/script.js
@@ -0,0 +1,12 @@
+var konto = -500;
+
+if (konto >= 0) {
+	alert("Das Konto ist positiv");
+	
+		if (Konto > 10000) {
+			alert("Es ist viel Geld vorhanden!");
+		}
+	}
+	else {
+		alert("Das Konto ist negativ");
+			}
