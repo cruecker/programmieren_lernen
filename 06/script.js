7d4968716bef2197bdc31b630dcea15267048d58
Claudius <58847225+cruecker@users.noreply.github.com>
2021-07-21 13:39:14 +0200

    Create script.js

---
+++ 06/script.js
@@ -0,0 +1,16 @@
+//switch anstatt x if zeilen
+var farbe = "gelb";
+
+switch ( farbe ) {
+	case "rot":
+			alert("Der Preis 4 Euro");
+	break;
+	case "blau":
+			alert("Der Preis 5 Euro");
+	break;
+	case "gelb":
+			alert("Der Preis 6 Euro");
+	break;
+	default:
+			alert("Diese Farbe gibt es nicht.");
+}
