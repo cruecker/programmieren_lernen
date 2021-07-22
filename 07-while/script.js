80d06a86f56168d601ce54345afdf3efd2cfea97
Claudius <58847225+cruecker@users.noreply.github.com>
2021-07-22 08:47:53 +0200

    Create script.js

---
+++ 07-while/script.js
@@ -0,0 +1,15 @@
+var menge = 0;
+
+//erzeuge den index
+var i = 0;
+
+//überprüfe die bedinung
+while (i < 10) {
+	menge = menge + 100;
+	
+	//erhöhe den index
+	i++;
+}
+
+//Ausgabe
+alert("Die Menge ist " + menge);
