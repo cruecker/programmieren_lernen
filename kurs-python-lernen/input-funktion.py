e3603ef55312352345ab31c65aa6bffec6d08157
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-01 17:49:31 +0200

    Create input-funktion.py

---
+++ kurs-python-lernen/input-funktion.py
@@ -0,0 +1,13 @@
+#das ist der Nachname
+name = input("Bitte den Nachnamen eingeben: ")
+
+#das ist der vorname
+vorname = input("Bitte den Vornamen eingeben: ")
+
+'''
+Das ist das
+alter
+'''
+alter = input("Bitte das Alter eingeben: ")
+
+print(vorname, name, alter)
