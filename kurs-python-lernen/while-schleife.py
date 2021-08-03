d51ffe6b5c6802049c7fe9fac39135fc92ab7ea2
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-03 18:00:09 +0200

    Create while-schleife.py

---
+++ kurs-python-lernen/while-schleife.py
@@ -0,0 +1,8 @@
+#i Name des Zählers wenn i verwendet j nehmen dann k
+#Schleifenzähler immer nur um 1 erhöhen
+i = 0
+while (i < 9):
+    i+= 1
+    print("Wert des Schleifenzählers",  i)
+print("Schleife beendet")
+    
