74b1dcb564dfbb7a00c78c0ae79ee9b972f1c1f5
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-03 17:50:23 +0200

    Create if-elif.py

---
+++ kurs-python-lernen/if-elif.py
@@ -0,0 +1,11 @@
+a = 3
+if a < 2:
+    print("Der Wert ist kleiner als 2")
+    print("Auch das wird bedingt ausgeführt")
+#elif = kurzform von else if
+elif a == 3:
+    print("Der Wert ist 3")
+else:
+    #bedingungen müssen nicht geklammert werden
+    if (a == 4):
+        print("Der Wert ist 4")
