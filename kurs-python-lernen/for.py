233ae9bdf3b63c367affef4a95dfc3e5c9cfebc3
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-03 18:07:04 +0200

    Create for.py

---
+++ kurs-python-lernen/for.py
@@ -0,0 +1,8 @@
+#for entspricht for-each in ps
+#zv ist keine zählvariable daher kein i
+zitat = "werft den purschen zu poden"
+zv = 0
+for x in zitat:
+    zv += 1
+    print("Zeichen ", zv, ":\t", x)
+print("Schleife beendet")
