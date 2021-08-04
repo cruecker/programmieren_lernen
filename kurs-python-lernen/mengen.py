be085fcf6be571d13079b4b9d2ed05d077a62066
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-04 17:11:28 +0200

    Create mengen.py

---
+++ kurs-python-lernen/mengen.py
@@ -0,0 +1,15 @@
+#menge definieren
+menge1 = set()
+print(menge1)
+menge2 = set("Graz")
+print(menge2)
+menge1.add(1)
+print(menge1)
+print(menge2.pop())
+print(menge2)
+menge2.clear()
+print(menge2)
+
+#kurz form der mengen definition
+menge3 = {"Graz","Frankfurt"}
+print(menge3)
