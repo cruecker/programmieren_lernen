7537b43332a7db04f8b7a8e8f2310a1f4f26ac16
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-04 16:40:00 +0200

    Create mehrereEinträgeInTupleAnsprechen.py

---
+++ kurs-python-lernen/mehrereEinträgeInTupleAnsprechen.py
@@ -0,0 +1,7 @@
+text = "Spam. Wonderfull spam."
+primzahlen = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31)
+wochentage = ("Mo","Di","Mi","Do","Fr","Sa","So")
+matrix =((1, 2), (3, 4))
+
+print(text[2:8])
+print(text[2:len(text)])
