5fd52858aa025811a15e411880b6766ef6f90ade
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-04 16:34:40 +0200

    Create einzelneEinträgeInTupleAnsprechen.py

---
+++ kurs-python-lernen/einzelneEinträgeInTupleAnsprechen.py
@@ -0,0 +1,9 @@
+text = "Spam. Wonderfull spam."
+primzahlen = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31)
+wochentage = ("Mo","Di","Mi","Do","Fr","Sa","So")
+matrix =((1, 2), (3, 4))
+
+print(text[2])
+print(primzahlen[5])
+print(matrix[0][1])
+print(wochentage[1])
