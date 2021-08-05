7e3ab59358e5789de8d5c83d9855ce11b63428a1
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-05 08:18:09 +0200

    Create klasse-katze.py

---
+++ kurs-python-lernen/klasse-katze.py
@@ -0,0 +1,16 @@
+#definieren einer Klasse
+class Katze:
+    Name = "Nemo"
+    Alter = 15
+    def lautgeben(self):
+        print("Miau!")
+
+#constructor erstellen, muss immer gemacht werden
+obj = Katze()
+print(type(obj))
+
+#aufrufen der funktion lautgeben
+obj.lautgeben()
+
+#aufrufen der eigenschaft alter
+print(obj.Alter)
