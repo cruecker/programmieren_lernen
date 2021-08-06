7f149349a465106250acf9803f9fbec71373b674
Claudius <58847225+cruecker@users.noreply.github.com>
2021-08-06 16:05:00 +0200

    funktionen hinzugefügt

--- Python-3-Grundkurs/main.py
+++ Python-3-Grundkurs/main.py
@@ -1,34 +1,53 @@
 #Main - Das ist ein kleines Beispiel Programm
-
-name = input("Bitte Ihren Namen eingaben: ")
-print("Hallo", name)
-
-groesse=input('Körpergrösse: ')
-
-
-datenspeicher={}
-while True:
+#funktion definieren immer mit eigenen Variablen in einern funktion arbeiten
+def rechnen(gr):
     gewicht=input('Gewicht: ')
     if not gewicht:
-        break
-    try:
-        bmi=round(float(gewicht)/(float(groesse)**2),2)
-    except ValueError:
-        continue
-    print('BMI: ', bmi)
-    if bmi>=25:
+        return
+    return round(float(gewicht)/(float(gr)**2),2)
+
+def auswerten(b):
+    if b>=25:
         print('Übergewicht')
-    elif bmi<18.5:
+    elif b<18.5:
         print('Untergewicht')
     else:
         print('Normalgewicht')
+
+def hinzufuegen(n,b):
     #prüfen ob der Name vorhanden ist und hinzufügen
-    if name in datenspeicher:
-        bmis=datenspeicher[name]
+    if n in datenspeicher:
+        bmis=datenspeicher[n]
     else:
         bmis=[]
-    bmis.append(bmi)
-    datenspeicher.update({name:bmis})
-#ausgabe des Inhalt des Dicts
-for i in datenspeicher.items():
-    print(i)
+    bmis.append(b)
+    datenspeicher.update({n:bmis})
+
+def ausgeben():
+    #ausgabe des Inhalt des Dicts
+    for i in datenspeicher.items():
+        print(i)
+
+#Name eingeben
+name = input("Bitte Ihren Namen eingaben: ")
+print("Hallo", name)
+
+#grösse eingeben
+groesse=input('Körpergrösse: ')
+
+#dictionary definieren
+datenspeicher={}
+
+while True:
+    try:
+        #Funktion rechnen aufrufen
+        bmi=rechnen(groesse)
+        if not bmi:
+            break
+    except ValueError:
+        continue
+
+    #Funktion auswerten aufrufen
+    auswerten(bmi)
+    hinzufuegen(name,bmi)
+ausgeben()
